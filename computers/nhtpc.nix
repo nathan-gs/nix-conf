de40d3691318bbf06ee0340bdd72f84bd6e7b8cf
Nathan Bijnens <nathan@nathan.gs>
2021-10-17 13:25:43 +0200

    Enable Flakes

--- computers/nhtpc.nix
+++ computers/nhtpc.nix
@@ -18,6 +18,14 @@
       ../monitoring.nix
     ];
 
+  
+  # Install the flakes edition
+  nix.package = pkgs.nixFlakes;
+  # Enable the nix 2.0 CLI and flakes support feature-flags
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes 
+  '';
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
