8f5d64a3164829318f90d2e223ea903ff8277c44
Nathan Bijnens <nathan@nathan.gs>
2021-11-14 18:14:51 +0100

    Photoprism: working config (except for Tensorflow)

--- media.nix
+++ media.nix
@@ -35,11 +35,37 @@
     keyFile = true;
   };
 
-  fileSystems."/var/lib/photoprism/originals/fn-fotos" = {
+  systemd.services.photoprism.serviceConfig.RestrictAddressFamilies = lib.mkForce "AF_UNIX AF_INET AF_INET6 AF_NETLINK";
+  systemd.services.photoprism.serviceConfig.SystemCallFilter = lib.mkForce [ "@system-service" "@network-io" "~@privileged" "~@resources" ];
+  systemd.services.photoprism.path = lib.mkForce [ pkgs.darktable pkgs.ffmpeg pkgs.exiftool ];
+  systemd.services.photoprism.environment.LD_LIBRARY_PATH=
+    (pkgs.libtensorflow-bin.overrideAttrs (oA: {
+       # 21.05 does not have libtensorflow-bin 1.x anymore & photoprism isn't compatible with tensorflow 2.x yet
+       # https://github.com/photoprism/photoprism/issues/222
+       src = pkgs.fetchurl {
+         url = "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-1.15.2.tar.gz";
+         sha256 = "04bi3ijq4sbb8c5vk964zlv0j9mrjnzzxd9q9knq3h273nc1a36k";
+       };
+       version = "1.15.2";
+     }));
+
+  
+  systemd.services.photoprism.confinement.enable = lib.mkForce false;
+
+  fileSystems."/var/lib/photoprism/originals" = {
     device = "/media/documents/nathan/onedrive_nathan_personal/fn-fotos";
     options = [ "bind" ];
   };
 
+  fileSystems."/var/lib/photoprism/import/femke-cameraroll" = {
+    device = "/media/documents/nathan/onedrive_nathan_personal/Camera Roll";
+    options = [ "bind" ];
+  };
+
+  fileSystems."/var/lib/photoprism/import/nathan-cameraroll" = {
+    device = "/media/documents/nathan/onedrive_nathan_personal/Pictures/Camera Roll";
+    options = [ "bind" ];
+  };
 
   networking.firewall.allowedTCPPorts = [ 2342 ];
  
