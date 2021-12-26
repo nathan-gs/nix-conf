a5359a5e395687395fb90dc981722d014529c0fe
Nathan Bijnens <nathan@nathan.gs>
2021-12-26 15:39:49 +0000

    nnas: flake ready

--- computers/nnas.nix
+++ computers/nnas.nix
@@ -2,12 +2,12 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, modulesPath, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
-      <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+      (modulesPath + "/installer/scan/not-detected.nix")
       ../headless.nix
       ../system.nix
       ../disks.nix
