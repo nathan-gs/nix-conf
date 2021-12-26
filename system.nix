dda603b09e5b01e34bd8a88de2e0c8e796006bef
Nathan Bijnens <nathan@nathan.gs>
2021-12-26 16:00:32 +0000

    flakes: move to system

--- system.nix
+++ system.nix
@@ -8,6 +8,13 @@
     ./smb.nix
   ];
 
+  # Install the flakes edition
+  nix.package = pkgs.nixFlakes;
+  # Enable the nix 2.0 CLI and flakes support feature-flags
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes 
+  '';
+
   time.timeZone = "Europe/Brussels";
 
   services.timesyncd.enable = true;
