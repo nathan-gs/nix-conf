6c60ba6074ebd47b7f8b72c1d8f634694ee358e7
Nathan Bijnens <nathan@nathan.gs>
2021-12-26 17:24:40 +0000

    Removed wget from required software

--- software.nix
+++ software.nix
@@ -5,7 +5,6 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-     wget
      gitMinimal
      iotop
      htop
