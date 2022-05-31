edd0588a59406f7ab3d481cd145976c03ca4ca53
Nathan Bijnens <nathan@nathan.gs>
2022-05-31 19:56:23 +0200

    Adding Monitoring for new Desktop

--- monitoring.nix
+++ monitoring.nix
@@ -12,6 +12,7 @@
               "nhtpc.wg:${toString config.services.prometheus.exporters.node.port}" 
               "nhtpc.wg:${toString config.services.prometheus.exporters.smokeping.port}"
               "nnas.wg:${toString config.services.prometheus.exporters.node.port}"
+              "ndesk:4445"
             ];
           }
         ];
