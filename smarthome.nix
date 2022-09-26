ac5f149a4ecd25d951723f0590bd0c6d35dc5d62
Nathan Bijnens <nathan@nathan.gs>
2022-09-26 18:25:26 +0000

    SmartHome: mqtt config

--- smarthome.nix
+++ smarthome.nix
@@ -4,7 +4,24 @@
 {
     services.mosquitto = {
         enable = true;
+        listeners.smarthome = {
+            users = {
+                homeassistant = {
+                    acl = ["readwrite /#"];
+                    password = config.secrets.mosquitto.users.homeassistant.password;
+                };
+                zigbee2mqtt = {
+                    acl = ["readwrite zigbee2mqtt/#"];
+                    password = config.secrets.mosquitto.users.zigbee2mqtt.password;
+                };
+            };
+            port = 1883;
+            
+        };
+
     };
+
+    networking.firewall.allowedTCPPorts = [ 1883 8080 ];
 
     services.home-assistant = {
         enable  = true;
@@ -37,6 +54,13 @@
             homeassistant =  config.services.home-assistant.enable;
             permit_join = true;
             serial.port = "/dev/ttyACM1";
+            frontend = true;
+
+            mqtt = {
+                server = "mqtt://localhost:1883";
+                user = "zigbee2mqtt";
+                password =  config.secrets.mosquitto.users.zigbee2mqtt.password;
+            };
         };
     };
 }
