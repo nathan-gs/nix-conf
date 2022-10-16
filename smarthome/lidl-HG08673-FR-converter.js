b73f03946ce92ce7126b41209736eed32e0e39b5
Nathan Bijnens <nathan@nathan.gs>
2022-10-16 15:00:32 +0000

    Smarthome: workaround for Lidl plugs (copied tuya version)

--- smarthome/lidl-HG08673-FR-converter.js
+++ smarthome/lidl-HG08673-FR-converter.js
@@ -10,19 +10,33 @@ const e = exposes.presets;
 const ea = exposes.access;
 
 const definition = {
-    fingerprint: [{modelID: 'TS011F', manufacturerName: '_TZ3000_ynmowqk2'}, {modelID: 'TS011F', manufacturerName: '_TZ3000_r6buo8ba'}],
-    model: 'HG08673-FR',
-    vendor: 'Lidl',
-    description: 'Silvercrest smart plug FR with power monitoring',
-    fromZigbee: [tuya.fzDataPoints],
-    toZigbee: [tuya.tzDataPoints],
-    exposes: [e.energy()],
-    configure: tuya.configureMagicPacket,
-    meta: {
-        tuyaDatapoints: [
-            [17, 'energy', tuya.valueConverter.divideBy100],
-        ],
-    },
+  fingerprint: [{modelID: 'TS011F', manufacturerName: '_TZ3000_ynmowqk2'}],
+  model: 'HG08673-FR',
+  vendor: 'Lidl',
+  description: 'Silvercrest smart plug FR with power monitoring',
+  ota: ota.zigbeeOTA,
+  fromZigbee: [fz.on_off, fzLocal.electrical_measurement_skip_duplicate, fzLocal.metering_skip_duplicate, fz.ignore_basic_report,
+    fz.tuya_switch_power_outage_memory, fz.ts011f_plug_indicator_mode, fz.ts011f_plug_child_mode],
+  toZigbee: [tz.on_off, tz.tuya_switch_power_outage_memory, tz.ts011f_plug_indicator_mode, tz.ts011f_plug_child_mode],
+  configure: async (device, coordinatorEndpoint, logger) => {
+    const endpoint = device.getEndpoint(1);
+    await endpoint.read('genBasic', ['manufacturerName', 'zclVersion', 'appVersion', 'modelId', 'powerSource', 0xfffe]);
+    await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'haElectricalMeasurement', 'seMetering']);
+    await reporting.rmsVoltage(endpoint, {change: 5});
+    await reporting.rmsCurrent(endpoint, {change: 50});
+    await reporting.activePower(endpoint, {change: 10});
+    await reporting.currentSummDelivered(endpoint);
+    endpoint.saveClusterAttributeKeyValue('haElectricalMeasurement', {acCurrentDivisor: 1000, acCurrentMultiplier: 1});
+    endpoint.saveClusterAttributeKeyValue('seMetering', {divisor: 100, multiplier: 1});
+    device.save();
+  },
+  exposes: [e.switch(), e.power(), e.current(), e.voltage().withAccess(ea.STATE),
+    e.energy(), exposes.enum('power_outage_memory', ea.ALL, ['on', 'off', 'restore'])
+        .withDescription('Recover state after power outage'),
+    exposes.enum('indicator_mode', ea.ALL, ['off', 'off/on', 'on/off', 'on'])
+        .withDescription('Plug LED indicator mode'), e.child_lock()],
 };
+
+
 
 module.exports = definition;
