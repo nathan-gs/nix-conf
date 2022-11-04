27c3eab3d20f59ba15abf7449495573c91e95dce
Nathan Bijnens <nathan@nathan.gs>
2022-11-04 10:29:01 +0000

    ha: split up config - energy

--- smarthome/devices.nix
+++ smarthome/devices.nix
@@ -426,6 +426,7 @@ tvAutomations = [
 ];
 
 water = import ./water.nix;
+energy = import ./energy.nix;
 
 in 
 
@@ -490,70 +491,14 @@ with lib;
 
     };
 
-    config.sensor = [
-      {
-        platform = "scrape";
-        resource = "https://callmepower.be/nl/energie/leveranciers/octaplus/tarieven";
-        name = "energy_electricity_cost_peak_kwh_energycomponent";
-        select = "table :has(> th:contains(Dagtarief)) td";
-        unit_of_measurement = "€/kWh";
-        value_template = "{{ (value | float) / 100 }}";
-        scan_interval = 3600;
-      }
-      {
-        platform = "scrape";
-        resource = "https://callmepower.be/nl/energie/leveranciers/octaplus/tarieven";
-        name = "energy_electricity_cost_offpeak_kwh_energycomponent";
-        select = "table :has(> th:contains(Nachttarief)) td";
-        unit_of_measurement = "€/kWh";
-        value_template = "{{ (value | float) / 100 }}";
-        scan_interval = 3600;
-      }
-      {
-        platform = "scrape";
-        resource = "https://callmepower.be/nl/energie/leveranciers/octaplus/tarieven";
-        name = "energy_gas_cost_kwh_energycomponent";
-        select = "table :has(> th:contains(per)) td";
-        unit_of_measurement = "€/kWh";
-        value_template = "{{ (value | float) / 100 }}";
-        scan_interval = 3600;
-      }      
-    ];
+    config.sensor = []
+      ++ energy.sensor;
 
     config.utility_meter = { } 
       // water.utility_meter;
 
-    config.template = 
-    [
-    
-      {
-        sensor = [
-          {
-            name = "energy_gas_cost_kwh";
-            unit_of_measurement = "€/kWh";
-            # Cost = vendor + federal tax + federal accijns + nettarifs + transport
-            state = "{{ ( states('sensor.energy_gas_cost_kwh_energycomponent') | float ) + (0.1058 / 100) + (0.0572 / 100) + (2.07 / 100) + (1.41 / 1000) }}";
-          }
-          {
-            name = "energy_gas_cost_m3";
-            unit_of_measurement = "€/m³";
-            state = "{{ ( states('sensor.energy_gas_cost_kwh') | float ) * 11.60}}";
-          }            
-          {
-            name = "energy_electricity_cost_peak_kwh";
-            unit_of_measurement = "€/kWh";
-            # Cost = vendor + federal accijns + nettarifs + transport + bijdrage energie + groene stroom + wkk
-            state = "{{ ( states('sensor.energy_electricity_cost_peak_kwh_energycomponent') | float ) + (1.44160 / 100) + (9.42 / 100) + (1.26 / 100) + (0.2942 / 100) + (2.233 / 100) + (0.344 / 100) }}";      
-          }
-          {
-            name = "energy_electricity_cost_offpeak_kwh";
-            unit_of_measurement = "€/kWh";
-            # Cost = vendor + federal accijns + nettarifs + transport + bijdrage energie + groene stroom + wkk
-            state = "{{ ( states('sensor.energy_electricity_cost_offpeak_kwh_energycomponent') | float ) + (1.44160 / 100) + (6.87 / 100) + (1.26 / 100) + (0.2942 / 100) + (2.233 / 100) + (0.344 / 100) }}";      
-          }
-        ];
-      }
-    ];
+    config.template = [] 
+      ++ energy.template;
 
     config.device_tracker = [
       {
