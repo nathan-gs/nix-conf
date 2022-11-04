755043d6216b5e42fe36d20ed29eb10c82461c7a
Nathan Bijnens <nathan@nathan.gs>
2022-11-04 09:23:07 +0000

    ha: refine water

--- smarthome/devices.nix
+++ smarthome/devices.nix
@@ -433,17 +433,22 @@ water.mqtt = {
     }
   ];
 
-  sensor = [
+  sensor = [    
     {
       name = "watermeter_total";
       state_topic = "watermeter/reading/current_value";
+      unique_id = name;
+      state_class = "total_increasing";
       unit_of_measurement = "L";
       force_update = true;
+      icon = "mdi:water";
     }
     {
       name = "watermeter_usage_last_minute";
       state_topic = "watermeter/reading/water_used_last_minute";
       unit_of_measurement = "L";
+      icon = "mdi:water";
+      unique_id = name;
     }
   ];
 
