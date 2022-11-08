713774f8dc3d730707a937682b9b8bb96b88b815
Nathan Bijnens <nathan@nathan.gs>
2022-11-08 18:02:52 +0000

    HA: water cost

--- smarthome/water.nix
+++ smarthome/water.nix
@@ -61,13 +61,13 @@
           {
             name = "water_cost";
             unit_of_measurement = "€/L";
-            state = """
+            state = ''
 {% if states('sensor.water_delivery_yearly') > ((1 * 30 + 4 * 30) * 1000) %}
 {{ (9.6434 / 1000) * 1.06 }}
 {% else %}
 {{ (4.5451 / 1000) * 1.06 }}
             {% endif %}
-            """;
+            '';
           }
       ];
     }
