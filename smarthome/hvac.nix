e7a840e6d383c208e9a1152ded53963305fa814c
Nathan Bijnens <nathan@nathan.gs>
2022-11-19 14:41:44 +0000

    HVAC: set temp on state change

--- smarthome/hvac.nix
+++ smarthome/hvac.nix
@@ -166,12 +166,43 @@ let
     })
     (map (v: v //  { type = "window";}) windows);
 
+  temperatureSetWorkaroundAutomations = 
+    map (v: {
+      id = "${v.floor}/${v.zone}/${v.type}/${v.name}.temperature_set_workaround";
+      alias = "${v.floor}/${v.zone}/${v.type}/${v.name} temperature_set_workaround";
+      trigger = [
+        {
+          platform = "state";
+          entity_id = "number.${v.floor}_${v.zone}_${v.type}_${v.name}_current_heating_setpoint_auto";
+        }
+      ];
+      condition = [
+        {
+          platform = "state";
+          entity_id = "climate.${v.floor}_${v.zone}_${v.type}_${v.name}";
+          state = "auto";
+        }
+      ];
+      action = [
+        {
+	        service = "climate.set_temperature";
+          target.entity_id = "climate.${v.floor}_${v.zone}_${v.type}_${v.name}";
+          data = {
+            temperature = "{{ states('number.${v.floor}_${v.zone}_${v.type}_${v.name}_current_heating_setpoint_auto') }}";
+          };
+        }        
+      ];
+      mode = "single";
+    })
+    (map (v: v //  { type = "rtv";}) rtvDevices);
+
 in
 {
   devices = [] ++ map (v: v //  { type = "window";}) windows;
   zigbeeDevices = {} // rtvDevices;
   automations = []
     ++ windowOpenAutomations
-    ++ windowClosedAutomations;
+    ++ windowClosedAutomations
+    ++ temperatureSetWorkaroundAutomations;
 
 }
