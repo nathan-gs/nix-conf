e3caa5fd0ec18a06806affb114e478fe8725f72d
Nathan Bijnens <nathan@nathan.gs>
2022-11-19 15:22:09 +0000

    lidl rtv WIP

--- smarthome/lidl-368308_2010-converter.js
+++ smarthome/lidl-368308_2010-converter.js
@@ -411,7 +411,7 @@ module.exports = [
             await reporting.bind(endpoint, coordinatorEndpoint, ['genBasic']);
         },
         exposes: [
-            e.child_lock(), e.comfort_temperature(), e.eco_temperature(), e.battery_voltage(), tuya.exposes.errorStatus()
+            e.child_lock(), e.comfort_temperature(), e.eco_temperature(), e.battery_voltage(), tuya.exposes.errorStatus(),
             exposes.numeric('current_heating_setpoint_auto', ea.STATE_SET).withValueMin(0.5).withValueMax(29.5)
                 .withValueStep(0.5).withUnit('°C').withDescription('Temperature setpoint automatic'),
             exposes.climate().withSetpoint('current_heating_setpoint', 0.5, 29.5, 0.5, ea.STATE_SET)
