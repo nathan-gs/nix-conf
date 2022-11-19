2a2f02a48e432841c6f49ccf4a8b4a35c62438e1
Nathan Bijnens <nathan@nathan.gs>
2022-11-19 14:51:51 +0000

    HVAC: set temp on state change

--- smarthome/hvac.nix
+++ smarthome/hvac.nix
@@ -176,13 +176,7 @@ let
           entity_id = "number.${v.floor}_${v.zone}_${v.type}_${v.name}_current_heating_setpoint_auto";
         }
       ];
-      condition = [
-        {
-          platform = "state";
-          entity_id = "climate.${v.floor}_${v.zone}_${v.type}_${v.name}";
-          state = "auto";
-        }
-      ];
+      condition = "{{ ( states('climate.${v.floor}_${v.zone}_${v.type}_${v.name}') == "auto" ) }}";
       action = [
         {
 	        service = "climate.set_temperature";
