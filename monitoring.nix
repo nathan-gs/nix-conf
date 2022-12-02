2c1ef2b20d33d7547bc869814351aa1f82c4704f
Nathan Bijnens <nathan@nathan.gs>
2022-12-02 20:55:10 +0000

    22.11 switch to vmagent

--- monitoring.nix
+++ monitoring.nix
@@ -2,9 +2,9 @@
 {
   services.prometheus.exporters = {
     node = enable;
-  }
+  };
 
-  services.vmaagent = {
+  services.vmagent = {
     enable = true;
     remoteWriteUrl = "https://${config.secrets.grafanaCloud.api.username}:${config.secrets.grafanaCloud.api.key}@prometheus-prod-01-eu-west-0.grafana.net/api/prom/push";
     prometheusConfig = {
