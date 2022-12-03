b6ffd574ec8550ca0d5b3699738bc7d7e18b5bb2
Nathan Bijnens <nathan@nathan.gs>
2022-12-03 12:01:40 +0000

    Cirkel Lamp: on & off

--- smarthome/lights.nix
+++ smarthome/lights.nix
@@ -33,7 +33,63 @@ let
       ieee = "0x50325ffffe5ebbec";
       floor = "floor0";
     }
+    {
+      zone = "voordeur";
+      name = "cirkel";
+      ieee = "0xa4c138fd75fc9f62";
+      floor = "garden";
+    }
   ];
+
+  automations = [
+    {
+      id = "garden_voordeur_cirkel_on";
+      alias = "garden_voordeur_cirkel_on";
+      trigger = [
+        {
+          platform = "sun";
+          event = "sunset";
+          offset = "-00:30:00";          
+        }
+        {
+          platform = "sun";
+          event = "sunrise";
+          offset = "-00:30:00";          
+        }
+      ];
+      condition = [];
+      action = [
+        {
+          service = "switch.turn_on";
+          target.entity_id = "switch.garden_voordeur_light_plug_cirkel";	        
+        }
+      ];
+      mode = "single";
+    }
+    {
+      id = "garden_voordeur_cirkel_off";
+      alias = "garden_voordeur_cirkel_off";
+      trigger = [
+        {
+          platform = "time";
+          at = "23:00:00";    
+        }
+        {
+          platform = "time";
+          at = "09:00:00";    
+        }        
+      ];
+      condition = [];
+      action = [
+        {
+          service = "switch.turn_off";
+          target.entity_id = "switch.garden_voordeur_light_plug_cirkel";	        
+        }
+      ];
+      mode = "single";
+    }
+  ];
+
 
   lightPlugDevices = 
     builtins.listToAttrs ( 
