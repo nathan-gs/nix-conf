0bdba6b2033ddbd8e5aa10a63062f0407c1f73c7
Nathan Bijnens <nathan@nathan.gs>
2022-12-04 11:15:49 +0000

    HA: fix electricity_delivery_power_max_threshold_reached

--- smarthome/energy.nix
+++ smarthome/energy.nix
@@ -344,7 +344,7 @@ let
           name = "electricity_delivery_power_max_threshold_reached";
           delay_on = "00:02:00";
           delay_off = "00:01:00";
-          state = "{{ states('sensor.electricity_delivery') | float > 2800 }}";
+          state = "{{ (states('sensor.dsmr_reading_electricity_currently_delivered') | float * 1000) > 2800 }}";
         };
       }
     ];
