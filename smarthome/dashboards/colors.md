ce65268d0968c94cb5f907b3c35aa81b48fff9cc
Nathan Bijnens <nathan@nathan.gs>
2022-12-25 13:30:44 +0100

    HA: hvac related tweaks

---
+++ smarthome/dashboards/colors.md
@@ -0,0 +1,4 @@
+# Dashboard Colors
+
+https://colorkit.co/color-palette-generator/1a4c6e-2772a5-3498db-ff9800-f3721e-e74c3c/
+
--- smarthome/hvac.nix
+++ smarthome/hvac.nix
@@ -92,6 +92,12 @@ let
       floor = "floor0";
     }
     {
+      zone = "keuken";
+      name = "na";
+      ieee = "0x00124b0029114a89";
+      floor = "floor0";
+    }
+    {
       zone = "bureau";
       name = "na";
       ieee = "0x00124b0029113a29";
@@ -108,6 +114,30 @@ let
       zone = "fen";
       name = "na";
       floor = "floor1";
+    }
+    {
+      ieee = "0x00124b00291149da";
+      zone = "nikolai";
+      name = "na";
+      floor = "floor1";
+    }
+    {
+      ieee = "0x00124b00291150db";
+      zone = "morgane";
+      name = "na";
+      floor = "floor1";
+    }
+    {
+      ieee = "0x00124b002910ed8a";
+      zone = "basement";
+      name = "na";
+      floor = "basement";
+    }
+    {
+      ieee = "0x00124b002910ed8a";
+      zone = "garden";
+      name = "noordkant";
+      floor = "garden";
     }
   ];
 
