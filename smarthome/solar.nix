1e21b925a13d121b684d95fddbefec0119127a4c
Nathan Bijnens <nathan@nathan.gs>
2023-03-25 16:12:23 +0100

    Sensor

--- smarthome/solar.nix
+++ smarthome/solar.nix
@@ -2,50 +2,54 @@
 
 {
 
-  template = [
+  template = [  
     {
-      name = "solar_currently_produced";
-      state = ''        
-        {{ (( states('sensor.solar_solis_inverter_cgi').split(";")[4] | float ) / 1000 ) | float }}
-      '';      
-      unit_of_measurement = "kW";
-      #device_class = "power";
-      icon = "mdi:solar-panel";
-    }
-    {
-      name = "solar_delivery_daily";
-      state = ''        
-        {{ ( states('sensor.solar_solis_inverter_cgi').split(";")[5] | float ) }}
-      '';      
-      unit_of_measurement = "kWh";
-      #device_class = "energy";
-      icon = "mdi:solar-panel";
-    }
-    {
-      trigger = {
-        platform = "time";
-        minutes = "23:59:00";
-      };
       sensor = [
         {
-          name = "solar_delivery_total_till_yesterday";
+          name = "solar_currently_produced";
           state = ''        
-            {{ states('sensor.solar_delivery_total')| float(0) }}
+            {{ (( states('sensor.solar_solis_inverter_cgi').split(";")[4] | float ) / 1000 ) | float }}
+          '';      
+          unit_of_measurement = "kW";
+          #device_class = "power";
+          icon = "mdi:solar-panel";
+        }
+        {
+          name = "solar_delivery_daily";
+          state = ''        
+            {{ ( states('sensor.solar_solis_inverter_cgi').split(";")[5] | float ) }}
           '';      
           unit_of_measurement = "kWh";
           #device_class = "energy";
           icon = "mdi:solar-panel";
         }
+        {
+          trigger = {
+            platform = "time";
+            minutes = "23:59:00";
+          };
+          sensor = [
+            {
+              name = "solar_delivery_total_till_yesterday";
+              state = ''        
+                {{ states('sensor.solar_delivery_total')| float(0) }}
+              '';      
+              unit_of_measurement = "kWh";
+              #device_class = "energy";
+              icon = "mdi:solar-panel";
+            }
+          ];
+        }
+        {
+          name = "solar_delivery_total";
+          state = ''
+            {{ ( states('sensor.solar_delivery_total_till_yesterday') | float(0) ) + (states('sensor.solar_delivery_daily') | float(0) ) }}
+          '';
+          unit_of_measurement = "kWh";
+          #device_class = "energy";
+          icon = "mdi:solar-panel";
+        }
       ];
-    }
-    {
-      name = "solar_delivery_total";
-      state = ''
-        {{ ( states('sensor.solar_delivery_total_till_yesterday') | float(0) ) + (states('sensor.solar_delivery_daily') | float(0) ) }}
-      '';
-      unit_of_measurement = "kWh";
-      #device_class = "energy";
-      icon = "mdi:solar-panel";
     }
   ];
   sensor = [
