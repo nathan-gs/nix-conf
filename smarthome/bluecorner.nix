e05e1118e2b085dbd4299b6b4d051eadf9dbaa3e
Nathan Bijnens <nathan@nathan.gs>
2023-04-10 20:46:10 +0200

    HA: bluecorner

--- smarthome/bluecorner.nix
+++ smarthome/bluecorner.nix
@@ -3,7 +3,19 @@
 {
 
   template = [  
-
+    {
+      sensors = {
+        bluecorner_total = {
+          unit_of_measurement = "kWh";
+          device_class = "energy";
+          icon = "mdi:car-electric";
+          state_class = "total";
+          value_template = ''
+            {{ states('sensor.bluecorner_total')|float(0) + states('sensor.bluecorner_last_charging_session')|float }}
+          '';
+        };
+      };
+    }
   ];
   sensor = [
     {
@@ -14,6 +26,19 @@
       json_attributes = ["refresh_token" "access_token"];
       value_template = 1; # https://community.home-assistant.io/t/rest-sensor-state-max-length-is-255-characters/31807/10
     }    
+    {
+      platform = "rest";
+      resource = "https://api.bluecorner.be/blue/api//v3.1/session/BCO/sessionlist/filtered?pagesize=1&pagenumber=1&caching=0&filter=%7B%7D&type=0";
+      name = "bluecorner_last_charging_session";
+      scan_interval = 3600;
+      headers = {
+        Authorization = ''Bearer {{ state_attr('sensor.bluecorner_token', 'access_token')}} '';
+        Accept = "application/json";
+      };
+      json_attributes_path = ''$.data.records[0]'';
+      json_attributes = ["SessionId"];      
+      value_template = ''{{ value_json[data][records][0].Consumption | float}}'';
+    }  
   ];
   utility_meter = {};
   customize = {};
