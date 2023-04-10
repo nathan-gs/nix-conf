6f676ad96cfd30203a1f05cbd10ec411379219dd
Nathan Bijnens <nathan@nathan.gs>
2023-04-10 20:10:31 +0200

    HA: bluecorner

--- smarthome/bluecorner.nix
+++ smarthome/bluecorner.nix
@@ -8,10 +8,11 @@
   sensor = [
     {
       platform = "command_line";
-      command = ''${pkgs.curl}/bin/curl --silent 'https://oauth.bluecorner.be/connect/token' -X POST -H 'Content-Type: application/x-www-form-urlencoded' --data-raw 'request_type=si%3As&refresh_token={{ state_attr('sensor.bluecorner_token', 'refresh_token')}}&grant_type=refresh_token&client_id=BCCP' '';
+      command = ''${pkgs.curl}/bin/curl --silent 'https://oauth.bluecorner.be/connect/token' -X POST -H 'Content-Type: application/x-www-form-urlencoded' --data-raw 'request_type=si%3As&refresh_token={{ state_attr('sensor.bluecorner_token', 'refresh_token')}}&grant_type=refresh_token&client_id=BCCP' | ${pkgs.jq}/bin/jq '.refresh_token, .access_token' '';
       name = "bluecorner_token";
       scan_interval = 900;
       json_attributes = ["refresh_token" "access_token"];
+      value_template = 1; # https://community.home-assistant.io/t/rest-sensor-state-max-length-is-255-characters/31807/10
     }    
   ];
   utility_meter = {};
