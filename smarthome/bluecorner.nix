e158392af8ec12449576bc9e1268724378fb56df
Nathan Bijnens <nathan@nathan.gs>
2023-04-10 19:28:29 +0200

    HA: bluecorner

--- smarthome/bluecorner.nix
+++ smarthome/bluecorner.nix
@@ -8,7 +8,7 @@
   sensor = [
     {
       platform = "command_line";
-      command = ''python3 -c "import httpx; print(httpx.post('https://oauth.bluecorner.be/connect/token', data={'request_type':si:s', 'refresh_token':'{{ state_attr('sensor.bluecorner_token', 'refresh_token')}}', 'grant_type': 'refresh_token', 'client_id':'BCCP'}).json())"'';      
+      command = ''python3 -c "import httpx; print(httpx.post('https://oauth.bluecorner.be/connect/token', data={'request_type':'si:s', 'refresh_token':'{{ state_attr('sensor.bluecorner_token', 'refresh_token')}}', 'grant_type': 'refresh_token', 'client_id':'BCCP'}).json())"'';      
       name = "bluecorner_token";
       scan_interval = 900;
       json_attributes = ["refresh_token" "access_token"];
