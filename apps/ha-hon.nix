c08bcb81d98d3dd833a13182015f1d519c4d1bd7
Nathan Bijnens <nathan@nathan.gs>
2023-04-18 21:56:39 +0200

    HA: latest hon

--- apps/ha-hon.nix
+++ apps/ha-hon.nix
@@ -1,14 +1,23 @@
-{ stdenv, pkgs, fetchFromGitHub }:
+{ lib, stdenv, pkgs, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
   name = "ha-hon";
   src = fetchFromGitHub {
     owner = "gvigroux";
     repo = "hon";
-    rev = "0.3.0";
-    sha256 =  "Vj6nPx1D7nFFG5OxMfFBX8vGmRG/uom7pvwnTjC+YM4=";
+    #rev = "0.3.0";
+    rev = "4abe91331bea02ce13e2d477901d734c39799573";
+    sha256 =  "G5Xprax4hwd07mks7OKJRdNu+s8C7dtu+Tn/965GWXY=";
   };
 
   
   installPhase = ''cp -a custom_components/hon $out'';
+
+  meta = with lib; {
+   description = "Home Assistant component supporting hOn cloud.";
+   homepage = "https://github.com/gvigroux/hon";
+   license = licenses.asl20;
+   maintainers = with maintainers; [ nathan-gs ];
+ };
+  
 }
