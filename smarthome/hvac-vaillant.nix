a5079c4fac82f8ea833e249f956d940cd3c31fc8
Nathan Bijnens <nathan@nathan.gs>
2023-07-13 01:23:47 +0200

    HA: Vaillant

--- smarthome/hvac-vaillant.nix
+++ smarthome/hvac-vaillant.nix
@@ -28,13 +28,13 @@
       temp_step = 0.5;
       modes = [ "auto" "heat" "cool" "off"];
       mode_state_template = ''
-        {% set values = { 'auto':'auto', 'heat':'on',  'cool':'night', 'off':'summer'} %}
+        {% set values = { 'auto':'auto', 'on':'heat',  'night':'cool', 'summer':'off'} %}
         {% set v = value_json.0.value %}
         {{ values[v] if v in values.keys() else 'auto' }}
       '';
       mode_state_topic = "ebusd/370/Hc1OPMode";
       mode_command_template = ''
-        {% set values = { 'auto':'auto', 'on':'heat',  'night':'cool', 'summer':'off'} %}       
+        {% set values = { 'auto':'auto', 'heat':'on',  'cool':'night', 'off':'summer'} %}
         { 
           "0": {
             "name": "",
@@ -44,7 +44,7 @@
       '';
       mode_command_topic = "ebusd/370/Hc1OPMode/set";
       temperature_state_topic = "ebusd/370/DisplayedHc1RoomTempDesired";
-      temperature_state_template = = "{{ value_json.temp1.value }}";
+      temperature_state_template = "{{ value_json.temp1.value }}";
       temperature_low_state_topic = "ebusd/370/Hc1NightTemp";
       temperature_low_state_template = "{{ value_json.temp1.value }}";
       temperature_high_state_topic = "ebusd/370/Hc1DayTemp";
