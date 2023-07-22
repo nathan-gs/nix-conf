ac62591569ca5d9799ed0343fcd3072800fdbf87
Nathan Bijnens <nathan@nathan.gs>
2023-07-22 11:48:54 +0200

    plex: expose

--- media.nix
+++ media.nix
@@ -27,6 +27,24 @@
     serviceConfig.ExecStart = lib.mkOverride 0 "${config.services.plex.package}/bin/plexmediaserver > /dev/null";
   };
 
+  services.nginx.virtualHosts."plex.nathan.gs" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:32400";
+      proxyWebsockets = true; # needed if you need to use WebSocket
+      extraConfig =
+        ''
+          # required when the target is also TLS server with multiple hosts
+          proxy_ssl_server_name on;
+          # required when the server wants to use HTTP Authentication
+          proxy_pass_header Authorization;
+          
+          proxy_buffering off;          
+        '';
+    };
+  };
+
 
   services.photoprism = {
     enable = true;
