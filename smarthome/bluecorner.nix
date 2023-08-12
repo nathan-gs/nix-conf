c7434b7b5553dd792eec5279529b114e5d36c062
Nathan Bijnens <nathan@nathan.gs>
2023-08-12 22:35:09 +0200

    HA: bluecorner - check month instead of individual sessions

--- smarthome/bluecorner.nix
+++ smarthome/bluecorner.nix
@@ -6,7 +6,7 @@
     {
       trigger = {
         platform = "state";
-        entity_id = "sensor.bluecorner_last_charging_session";
+        entity_id = "sensor.bluecorner_current_month";
       };
       sensor = [
         {
@@ -15,9 +15,30 @@
           device_class = "energy";
           icon = "mdi:car-electric";
           state_class = "total";
-          state = ''            
-            {{ states('sensor.bluecorner_total')|float(0) + ( states('sensor.bluecorner_last_charging_session') | float(0) ) }}
+          state = ''
+            {{ states('sensor.bluecorner_total_till_last_month')|float(0) + ( states('sensor.bluecorner_current_month') | float(0) ) }}
           '';
+        }
+      ];
+    }
+    {
+      trigger = {
+        platform = "time";
+        at = "00:00:00";
+      };
+      sensor = [
+        {
+          name = "bluecorner_total_till_last_month";
+          state = ''
+            {% if now().day == 1  %}
+              {{ states('sensor.bluecorner_total')| float(0) }}
+            {% else %}
+              {{ states('bluecorner_total_till_last_month') | float(0) }}
+            {% endif %}            
+          '';
+          unit_of_measurement = "kWh";
+          device_class = "energy";
+          icon = "mdi:car-electric";
         }
       ];
     }
@@ -33,20 +54,18 @@
     }    
     {
       platform = "rest";
-      resource = "https://api.bluecorner.be/blue/api//v3.1/session/BCO/sessionlist/filtered?pagesize=1&pagenumber=1&caching=0&filter=%7B%7D&type=0";
-      name = "bluecorner_last_charging_session";
+      resource = "https://api.bluecorner.be/blue/api/v3.1/indicator/BCO/KWHMyChargers";
+      name = "bluecorner_current_month";
       scan_interval = 900;
       headers = {
         Authorization = ''Bearer {{ state_attr('sensor.bluecorner_token', 'access_token')}} '';
         Accept = "application/json";
-      };
-      json_attributes_path = ''$.data.records[0]'';
-      json_attributes = ["SessionId"];      
-      value_template = ''{{ value_json.data.records[0].Consumption | float / 1000 }}'';
+      };      
+      value_template = ''{{ value_json.data[-1].value | float / 1000 }}'';
       unit_of_measurement = "kWh";
       device_class = "energy";
       icon = "mdi:car-electric";
-    }  
+    }
   ];
 
   mqtt_sensor =  [
