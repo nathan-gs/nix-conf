9d6a242287d14ce3a97a065bb223ee312f9640ab
Nathan Bijnens <nathan@nathan.gs>
2023-10-15 16:10:49 +0200

    HA/PowerCalc: speedier updates

--- smarthome/energy/powercalc.nix
+++ smarthome/energy/powercalc.nix
@@ -1,4 +1,4 @@
-{config, pkgs, lib, ...}:
+{ config, pkgs, lib, ... }:
 
 {
 
@@ -7,7 +7,7 @@
       enable_autodiscovery = false;
       energy_sensor_naming = "{}_energy";
       power_sensor_naming = "{}_power";
-
+      force_update_frequency = "00:01:00";
       sensors = [
       ];
     };
