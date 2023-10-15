7b99ee546372761052b7bbf1ca0ac64e3586ccd5
Nathan Bijnens <nathan@nathan.gs>
2023-10-15 15:12:21 +0200

    HA: car charger

--- smarthome/energy/car_charger/volvo.nix
+++ smarthome/energy/car_charger/volvo.nix
@@ -1,5 +1,7 @@
 {config, pkgs, lib, ...}:
-
+let 
+  carName = config.secrets.nathan-car.name;
+in
 {
 
   services.home-assistant.config = {
@@ -7,8 +9,17 @@
       sensors = [
         {
           name = "car_charger";
-          entity_id = "binary_sensor.2abn528_battery_charging";
-          fixed.power = 1995;
+          entity_id = "binary_sensor.${carName}_battery_charging";
+          fixed.power = ''
+            {% set car_charging = states('binary_sensor.${carName}_battery_charging') | bool (false) %}
+            {% set charger = states('switch.garden_garden_plug_laadpaal') | bool (false) %}
+            {% set position = states('device_tracker.${carName}_position') == "home" %}
+            {% set power = 0 %}
+            {% if car_charging and charger and position %}
+              {% set power = 1995 %}
+            {% endif %}
+            {{ power }}
+          '';
         }
       ];
     };
