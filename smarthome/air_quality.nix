dac3f2bc1253a4e132eb5002e2b71ad23927e225
Nathan Bijnens <nathan@nathan.gs>
2023-12-21 21:26:25 +0100

    HA/Air Quality: sensors & dashboard

--- smarthome/air_quality.nix
+++ smarthome/air_quality.nix
@@ -56,6 +56,15 @@ let
     }
   ];
 
+  sensor = name: select: device_class: 
+    {
+      name = name;
+      select = select;
+      value_template = ''{{ value | float("unavailable") }}'';
+      unit_of_measurement = "µg/m³";
+      device_class = device_class;
+      state_class = "measurement";
+    };
 
 in
 {
@@ -67,65 +76,31 @@ in
         resource = "https://www.irceline.be/tables/no2/no2.php?lan=en";
         scan_interval = 3600;
         sensor = 
-        map (l: 
-          {
-            name = "irceline/${l.name}/air_quality/no2";
-            select = ''
-              table :has(> td:-soup-contains("${l.station}")) td[align=center] > :first-child :first-child
-            '';
-            value_template = "{{ value | float(-1) }}";
-            unit_of_measurement = "µg/m³";
-            device_class = "nitrogen_dioxide";
-          }) locations_no2 ++
-        map (l: {
-            name = "irceline/${l.name}/air_quality/no2/24h";
-            select = ''
-              table :has(> td:-soup-contains("${l.station}")) td:nth-child(5)
-            '';
-            value_template = "{{ (value | float(-1))  }}";
-            unit_of_measurement = "µg/m³";
-            device_class = "nitrogen_dioxide";
-          }) locations_no2;
+          map (l: 
+            sensor "irceline/${l.name}/air_quality/no2" ''table :has(> td:-soup-contains("${l.station}")) td[align=center] > :first-child :first-child'' "nitrogen_dioxide"
+          ) locations_no2 
+          ++
+          map (l: 
+            sensor "irceline/${l.name}/air_quality/no2/24h" ''table :has(> td:-soup-contains("${l.station}")) td:nth-child(5)'' "nitrogen_dioxide"
+          ) locations_no2;
       }
       {
         resource = "https://www.irceline.be/tables/ozone/ozone.php?lan=en";
         scan_interval = 3600;
-        sensor = map (l: (
-          {
-            name = "irceline/${l.name}/air_quality/ozone";
-            select = ''
-              table :has(> td:-soup-contains("${l.station}")) td:nth-child(4)
-            '';
-            value_template = "{{ value | float(-1) }}";
-            unit_of_measurement = "µg/m³";
-            device_class = "ozone";
-          }
-          )) locations_ozone;
+        sensor = map (l: 
+          sensor "irceline/${l.name}/air_quality/ozone" ''table :has(> td:-soup-contains("${l.station}")) td:nth-child(4)'' "ozone"
+        ) locations_ozone;
       }
       {
         resource = "https://www.irceline.be/tables/pm/pm10.php?lan=en";
         scan_interval = 3600;
         sensor = 
-        map (l: 
-          {
-            name = "irceline/${l.name}/air_quality/pm10";
-            select = ''
-              table :has(> td:-soup-contains("${l.station}")) td:nth-child(4)
-            '';
-            value_template = "{{ value | float(-1) }}";
-            unit_of_measurement = "µg/m³";
-            device_class = "pm10";
-          }) locations_pm10 ++
-        map (l: 
-          {
-            name = "irceline/${l.name}/air_quality/pm25";
-            select = ''
-              table :has(> td:-soup-contains("${l.station}")) td:nth-child(7)
-            '';
-            value_template = "{{ (value | float(-1))  }}";
-            unit_of_measurement = "µg/m³";
-            device_class = "pm25";
-          }
+          map (l: 
+            sensor "irceline/${l.name}/air_quality/pm10" ''table :has(> td:-soup-contains("${l.station}")) td:nth-child(4)'' "pm10"
+          ) locations_pm10 
+          ++
+          map (l: 
+            sensor "irceline/${l.name}/air_quality/pm25" ''table :has(> td:-soup-contains("${l.station}")) td:nth-child(7)'' "pm25"        
           ) locations_pm10;
       }
     ];
