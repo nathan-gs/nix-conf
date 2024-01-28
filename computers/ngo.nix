4c50e45e9e7b160d36471ab39a39f65cd3d22678
Nathan Bijnens <nathan@nathan.gs>
2024-01-28 19:50:14 +0100

    ngo: most basic config

--- computers/ngo.nix
+++ computers/ngo.nix
@@ -7,14 +7,15 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./ngo-hardware.nix
+      ../system.nix
     ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "ngo"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -66,16 +67,10 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.nathan = {
-    isNormalUser = true;
-    description = "Nathan";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
+  users.users.nathan.packages = with pkgs; [
+    firefox
     #  thunderbird
-    ];
-  };
+  ];
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
--- flake.nix
+++ flake.nix
@@ -5,17 +5,18 @@
     secrets.url = "git+file:///etc/nixos/secrets";
     photoprism-slideshow.url = "github:nathan-gs/photoprism-slideshow";
   };
-  
+
 
   outputs = { self, nixpkgs, nixpkgs-unstable, secrets, photoprism-slideshow, ... }:
-    let 
+    let
       overlay = final: prev: { nixpkgs-unstable = nixpkgs-unstable.legacyPackages.${prev.system}; };
-      overlayModule = ({ config, pkgs, ...}: { nixpkgs.overlays = [ overlay ]; });
+      overlayModule = ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay ]; });
 
-    in 
-      {  
-    
-        nixosConfigurations.nhtpc = nixpkgs.lib.nixosSystem {
+    in
+    {
+
+      nixosConfigurations = {
+        nhtpc = nixpkgs.lib.nixosSystem {
           modules = [
             # Point this to your original configuration.
             ./computers/nhtpc.nix
@@ -25,24 +26,39 @@
           ];
 
           specialArgs.channels = { inherit nixpkgs nixpkgs-unstable; };
-      
+
           # Select the target system here.
           system = "x86_64-linux";
         };
-  
 
-        nixosConfigurations.nnas = nixpkgs.lib.nixosSystem {
+        ngo = nixpkgs.lib.nixosSystem {
+          modules = [
+            # Point this to your original configuration.
+            ./computers/ngo.nix
+            secrets.nixosModules.secrets
+            overlayModule
+          ];
+
+          specialArgs.channels = { inherit nixpkgs nixpkgs-unstable; };
+
+          # Select the target system here.
+          system = "x86_64-linux";
+        };
+
+
+        nnas = nixpkgs.lib.nixosSystem {
           modules = [
             # Point this to your original configuration.
             ./computers/nnas.nix
             secrets.nixosModules.secrets
           ];
 
-      
+
           # Select the target system here.
           system = "x86_64-linux";
-       };
+        };
+      };
     };
-  }
+}
 
 
