3cc15553494fbaca7f3237fdcc6c68319e2ec679
Nathan Bijnens <nathan@nathan.gs>
2024-02-04 12:07:35 +0100

    HA: remove electrolux status because the repo disappeared

--- services/home-assistant.nix
+++ services/home-assistant.nix
@@ -135,9 +135,9 @@
     customComponents = [
       (pkgs.callPackage ../pkgs/home-assistant/custom_components/solis-sensor.nix {})
       (pkgs.callPackage ../pkgs/home-assistant/custom_components/hon.nix {})
-      (pkgs.callPackage ../pkgs/home-assistant/custom_components/electrolux-status.nix {
-        pyelectroluxconnect = (pkgs.python311Packages.callPackage ../pkgs/python/pyelectroluxconnect.nix {});
-      })
+      #(pkgs.callPackage ../pkgs/home-assistant/custom_components/electrolux-status.nix {
+      #  pyelectroluxconnect = (pkgs.python311Packages.callPackage ../pkgs/python/pyelectroluxconnect.nix {});
+      #})
       (pkgs.callPackage ../pkgs/home-assistant/custom_components/indego.nix {
         pyindego = (pkgs.python311Packages.callPackage ../pkgs/python/pyindego.nix {});
       })
