9fe3a3a9459d8fb7160e396f185870d9db250154
Nathan Bijnens <nathan@nathan.gs>
2024-02-17 19:23:04 +0100

    HA: update custom components

--- pkgs/home-assistant/custom_components/afvalbeheer.nix
+++ pkgs/home-assistant/custom_components/afvalbeheer.nix
@@ -4,13 +4,13 @@ buildHomeAssistantComponent  rec {
 
   owner = "pippyn";
   domain = "afvalbeheer";
-  version = "5.2.14";
+  version = "5.3.1";
 
   src = fetchFromGitHub {
     owner = "pippyn";
     repo = "Home-Assistant-Sensor-Afvalbeheer";
     rev = "v${version}";
-    sha256 = "sha256-fLwJ65z3jTwFb6CqMsOR6ANbmnwr93RQ8u9t1u8dkfs=";
+    sha256 = "sha256-o6LUBffUQkAStVfPgW0votpS3lflNbXRaI1P8aWGpMg=";
   };
 
   propagatedBuildInputs = [
--- pkgs/home-assistant/custom_components/powercalc.nix
+++ pkgs/home-assistant/custom_components/powercalc.nix
@@ -4,13 +4,13 @@ buildHomeAssistantComponent rec {
 
   owner = "bramstroker";
   domain = "powercalc";
-  version = "1.10.0";
+  version = "1.10.4";
 
   src = fetchFromGitHub {
     owner = owner;
     repo = "homeassistant-powercalc";
     rev = "refs/tags/v${version}";
-    hash = "sha256-C4gU9cmiUsFHdByGv3qR4pEAMO49CbpQYH4BsSOomnc=";
+    hash = "sha256-gWhCQ0xygXyy6mduH47OBLFjTlu1DFe5mw3lIHI2kds=";
   };
   
   propagatedBuildInputs = [
