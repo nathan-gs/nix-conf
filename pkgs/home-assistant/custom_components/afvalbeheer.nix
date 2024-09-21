717003201180c3dc5b54317b2d3db5455973dc9d
Nathan Bijnens <nathan@nathan.gs>
2024-09-21 11:03:30 +0200

    HA/RecycleApp: latest

--- pkgs/home-assistant/custom_components/afvalbeheer.nix
+++ pkgs/home-assistant/custom_components/afvalbeheer.nix
@@ -4,13 +4,13 @@ buildHomeAssistantComponent  rec {
 
   owner = "pippyn";
   domain = "afvalbeheer";
-  version = "5.3.4";
+  version = "5.4.3";
 
   src = fetchFromGitHub {
     owner = "pippyn";
     repo = "Home-Assistant-Sensor-Afvalbeheer";
     rev = "v${version}";
-    sha256 = "sha256-oJldq6aPlxud5b4QVoAsJNsv4ZO2wU1/X/Q7OjXLSng=";
+    sha256 = "sha256-8z8OClC6H4C5kqx0lHAEGzcFpyyyI4AZ3W2+g5+vU70=";
   };
 
   propagatedBuildInputs = [
