ca056721285d7db9a711f54bc98163813e7c5b2d
Nathan Bijnens <nathan@nathan.gs>
2024-09-30 23:45:15 +0200

    HA: update to tax calc for energy

--- smarthome/energy/tariffs.nix
+++ smarthome/energy/tariffs.nix
@@ -11,29 +11,88 @@
           {
             name = "gas_cost_kwh";
             unit_of_measurement = "€/kWh";
-            # Cost = vendor + federal tax + federal accijns + nettarifs + transport
-            state = "{{ ( states('sensor.gas_cost_kwh_energycomponent') | float ) + (0.1058 / 100) + (0.0572 / 100) + (2.07 / 100) + (1.41 / 1000) }}";
+            state = ''
+              {% set energycomponent = states('sensor.gas_cost_octa_kwh_energycomponent') | float %}
+              {% set gas_yearly = (states('sensor.gas_delivery_yearly') | float * 11.5822997166)  %}
+              {% set imewo = (0.887 / 100) %}
+              {% if gas_yearly < 5000 %}
+                {% set imewo = (2.454 / 100) %}
+              {% endif %}
+              {% set transport = (0.162 / 100) %}
+              {% set energiebijdrage = (0.10577 / 100) %}
+              {% set accijns = (0.97549 / 100) %}
+              {% if gas_yearly < 12000 %}
+                {% set accijns = (0.87238 / 100) %}
+              {% endif %}
+              {{ (energycomponent + imewo + transport + energiebijdrage + accijns) | round(5) }}
+            '';
             state_class = "measurement";
           }
           {
             name = "gas_cost_m3";
             unit_of_measurement = "€/m³";
-            state = "{{ ( states('sensor.gas_cost_kwh') | float ) * 11.60}}";
+            state = "{{ ( states('sensor.gas_cost_kwh') | float ) * 11.5822997166}}";
             state_class = "measurement";
-          }            
+          }
+          {
+            name = "gas_cost_monthly_fixed";
+            unit_of_measurement = "€";
+            state = ''
+              {% set gas_yearly = (states('sensor.gas_delivery_yearly') | float * 11.5822997166)  %}
+              {% set engie = 37.1 %}
+              {% set discount = 0 %}
+              {% if now().year < 2025 and now().month < 10 %}
+                {% set discount = 37.1 %}
+              {% endif %}
+              {% set imewo = 95.37 %}
+              {% if gas_yearly < 5000 %}
+                {% set imewo = 17.0 %}
+              {% endif %}
+              {% set databeheer = 13.95 %}
+              {{ (engie / 12) - (discount / 12) + (imewo / 12) + (databeheer / 12) }}
+            '';
+          }           
           {
             name = "electricity_cost_peak_kwh";
             unit_of_measurement = "€/kWh";
-            # Cost = vendor + federal accijns + nettarifs + transport + bijdrage energie + groene stroom + wkk
-            state = "{{ ( states('sensor.electricity_cost_peak_kwh_energycomponent') | float ) + (1.44160 / 100) + (9.42 / 100) + (1.26 / 100) + (0.2942 / 100) + (2.233 / 100) + (0.344 / 100) }}";      
+            state = ''
+              {% set energycomponent = states('sensor.electricity_cost_octa_peak_kwh_energycomponent') | float %}
+              {% set wkk_and_greenenergy = (1.582 / 100) %}
+              {% set imewo = (4.71756 / 100) %}
+              {% set energiebijdrage = (0.20417 / 100) %}
+              {% set accijns = (5.03288 / 100) %}
+              {{ energycomponent + wkk_and_greenenergy + imewo + energiebijdrage + accijns | round(5) }}
+            '';
             state_class = "measurement";
           }
           {
             name = "electricity_cost_offpeak_kwh";
-            unit_of_measurement = "€/kWh";
-            # Cost = vendor + federal accijns + nettarifs + transport + bijdrage energie + groene stroom + wkk
-            state = "{{ ( states('sensor.electricity_cost_offpeak_kwh_energycomponent') | float ) + (1.44160 / 100) + (6.87 / 100) + (1.26 / 100) + (0.2942 / 100) + (2.233 / 100) + (0.344 / 100) }}";      
+            unit_of_measurement = "€/kWh";            
+            state = ''
+              {% set energycomponent = states('sensor.electricity_cost_octa_offpeak_kwh_energycomponent') | float %}
+              {% set wkk_and_greenenergy = (1.582 / 100) %}
+              {% set imewo = (4.71756 / 100) %}
+              {% set energiebijdrage = (0.20417 / 100) %}
+              {% set accijns = (5.03288 / 100) %}
+              {{ energycomponent + wkk_and_greenenergy + imewo + energiebijdrage + accijns | round(5) }}
+            '';
             state_class = "measurement";
+          }
+          {
+            name = "electricity_cost_monthly_fixed";
+            unit_of_measurement = "€";
+            state = ''
+              {% set engie = 100.7 %}
+              {% set imewo_capacitytariffs_per_kw = 41.7713 %}
+              {# Not 100% correct, 1 peak influences full month #}
+              {% set capacity_peak_current_month = max(states('sensor.electricity_delivery_power_monthly_15m_max') | float(2.5), 2.5) %}
+              {% set discount = 0 %}
+              {% if now().year < 2025 and now().month < 10 %}
+                {% set discount = 80.0 %}
+              {% endif %}
+              {% set databeheer = 13.95 %}
+              {{ (engie / 12) + ((imewo_capacitytariffs_per_kw / 12 ) * capacity_peak_current_month) - (discount / 12) + (databeheer / 12) }}
+            '';
           }
           {
             name = "energy/electricity/cost";
@@ -46,7 +105,8 @@
               {% endif %}
             '';
             state_class = "measurement";
-          }
+          }          
+
         ];
         binary_sensor = [
           {
@@ -71,32 +131,87 @@
     ];
 
     scrape = [
-       {
+      {
         resource = "https://callmepower.be/nl/energie/leveranciers/octaplus/tarieven";
         scan_interval = 3600;
         sensor = [
           {
-            name = "electricity_cost_peak_kwh_energycomponent";
+            name = "electricity_cost_octa_peak_kwh_energycomponent";
             select = "table:has(th:-soup-contains(Clear)) tbody td li:-soup-contains(Dagtarief)";
             value_template = "{{ (value.split('€')[1]|replace(',', '.')|trim | float) / 100 }}";
             unit_of_measurement = "€/kWh";
             state_class = "measurement";
           }
           {
-            name = "electricity_cost_offpeak_kwh_energycomponent";
+            name = "electricity_cost_octa_offpeak_kwh_energycomponent";
             select = "table:has(th:-soup-contains(Clear)) tbody td li:-soup-contains(Nachttarief)";
             unit_of_measurement = "€/kWh";
             value_template = "{{ (value.split('€')[1]|replace(',', '.')|trim | float) / 100 }}";
             state_class = "measurement";
           }
           {
-            name = "gas_cost_kwh_energycomponent";
+            name = "gas_cost_octa_kwh_energycomponent";
             select = ''
               h3#octa-clear-gas + p + p + div table:has(th:-soup-contains(Clear)) tbody td li:-soup-contains(cent)
             '';
             unit_of_measurement = "€/kWh";
             value_template = ''
               {{ (value.split(':')[1].split('€')[1]|replace(',', '.')|trim | float) / 100 }}
+            '';
+            state_class = "measurement";
+          }
+        ];
+      }
+      {
+        resource = "https://callmepower.be/nl/energie/leveranciers/engie-electrabel/tarieven/drive";
+        scan_interval = 3600;
+        sensor = [
+          {
+            name = "electricity_cost_engie_drive_peak_kwh_energycomponent";
+            # TODO Sept 2025 replace the discount
+            select = ''
+              h2#actuele-engie-drive-prijzen-per-kwh + p + p + div table tbody td li:-soup-contains(Dagtarief)
+            '';
+            value_template = ''
+              {% set discount = 0 %}
+              {% if now().year < 2025 and now().month < 10 %}
+                {% set discount = 0.0293 %}
+              {% endif %}
+              {{ (((value.split('€')[1]|replace(',', '.')|trim | float) / 100) - discount) | round(5) }}
+            '';
+            unit_of_measurement = "€/kWh";
+            state_class = "measurement";
+          }
+          {
+            name = "electricity_cost_engie_drive_offpeak_kwh_energycomponent";
+            # TODO Sept 2025 replace the discount
+            select = ''
+              h2#actuele-engie-drive-prijzen-per-kwh + p + p + div table tbody td li:-soup-contains(Nachttarief)
+            '';
+            unit_of_measurement = "€/kWh";
+            value_template = 
+            ''
+              {% set discount = 0 %}
+              {% if now().year < 2025 and now().month < 10 %}
+                {% set discount = 0.0293 %}
+              {% endif %}
+              {{ (((value.split('€')[1]|replace(',', '.')|trim | float) / 100) - discount) | round(5) }}
+            '';
+            state_class = "measurement";
+          }
+          {
+            name = "gas_cost_engie_drive_kwh_energycomponent";
+            select = ''
+              h3#gasprijs-engie-drive + p + p + div table:has(th:-soup-contains(Drive)) tbody td li:-soup-contains(cent)
+            '';
+            unit_of_measurement = "€/kWh";
+            # TODO Sept 2025 replace the discount
+            value_template = ''
+              {% set discount = 0 %}
+              {% if now().year < 2025 and now().month < 10 %}
+                {% set discount = 0.0106 %}
+              {% endif %}
+              {{ (((value.split(':')[1].split('€')[1]|replace(',', '.')|trim | float) / 100) - discount) | round(5) }}
             '';
             state_class = "measurement";
           }
@@ -110,6 +225,24 @@
             name = "electricity_injection_kwh";
             select = ''
               table :has(> td:-soup-contains(Octa)) td:nth-child(3)
+            '' ;
+            value_template = "{{ (value | replace(',', '.') | float(-1) / 100) | round(5) }}";
+            unit_of_measurement = "€/kWh";
+            state_class = "measurement";
+          }
+          {
+            name = "electricity_injection_engie_drive_peak_kwh";
+            select = ''
+              table :has(> td:-soup-contains(Flow)) td:nth-child(4)
+            '' ;
+            value_template = "{{ (value | replace(',', '.') | float(-1) / 100) | round(5) }}";
+            unit_of_measurement = "€/kWh";
+            state_class = "measurement";
+          }
+          {
+            name = "electricity_injection_engie_drive_offpeak_kwh";
+            select = ''
+              table :has(> td:-soup-contains(Flow)) td:nth-child(5)
             '' ;
             value_template = "{{ (value | replace(',', '.') | float(-1) / 100) | round(5) }}";
             unit_of_measurement = "€/kWh";
