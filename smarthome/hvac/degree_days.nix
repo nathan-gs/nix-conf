4df097470ba53647328c26b9e6d3f2c2d3ce7150
Nathan Bijnens <nathan@nathan.gs>
2024-10-12 19:57:56 +0200

    HA: FIX degree days

--- smarthome/hvac/degree_days.nix
+++ smarthome/hvac/degree_days.nix
@@ -69,6 +69,15 @@
       }
     ];
 
+    recorder = {
+      include = {
+        entities = [
+          "sensor.degree_day_daily"
+          "sensor.gas_m3_per_degree_day"
+          "sensor.gas_m3_per_degree_day_occupancy_adjusted"
+        ];
+      };
+    };
     
   };
 }
