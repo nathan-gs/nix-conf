abd8877326f18fa4a5ee0b8df9e76d2a1965230d
Nathan Bijnens <nathan@nathan.gs>
2024-11-24 18:32:24 +0100

    HA: temperature tweaks

--- smarthome/hvac/temperature_sets.nix
+++ smarthome/hvac/temperature_sets.nix
@@ -1,9 +1,9 @@
 ''
-  {% set workday = states('binary_sensor.workday') | bool(true) %}    
-  {% set anyone_home = states('binary_sensor.anyone_home') | bool(true) %}
+  {% set workday = is_state('binary_sensor.workday', 'on') %}    
+  {% set anyone_home = is_state('binary_sensor.anyone_home', 'on') %}
   {% set temperature_away = 14.5 %}
-  {% set temperature_eco = 16 %}
-  {% set temperature_night = 17 %}
+  {% set temperature_eco = 15.5 %}
+  {% set temperature_night = 16 %}
   {% set temperature_comfort_low = 17.5 %}
   {% set temperature_comfort = 19 %}
   {% set temperature_minimal = 5.5 %}
