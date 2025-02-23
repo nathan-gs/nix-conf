9c641febe833fb2431d6f703b412181bb34079dc
Nathan Bijnens <nathan@nathan.gs>
2025-02-23 19:50:36 +0100

    HA: update HON

--- pkgs/home-assistant/custom_components/hon.nix
+++ pkgs/home-assistant/custom_components/hon.nix
@@ -3,21 +3,21 @@
 buildHomeAssistantComponent rec {
   owner = "gvigroux";
   domain = "hon";
-  version = "0.7.10";
+  version = "0.7.13";
 
   src = fetchFromGitHub {
     owner = "gvigroux";
     repo = "hon";
     rev = version;
-    sha256 = "sha256-1MNjqHj6MN21yOWZh1CBBNWTaLWmAaMuw3sO6VXfqKA=";
+    sha256 = "sha256-q3lNxcjMLfNumj8CusKvkVevuLR2zFBOnLw5FdHEE74=";
   };
 
 
   meta = with lib; {
-   description = "Home Assistant component supporting hOn cloud.";
-   homepage = "https://github.com/gvigroux/hon";
-   license = licenses.asl20;
-   maintainers = with maintainers; [ nathan-gs ];
- };
+    description = "Home Assistant component supporting hOn cloud.";
+    homepage = "https://github.com/gvigroux/hon";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ nathan-gs ];
+  };
   
 }
