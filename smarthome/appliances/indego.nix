41e1e078466b3ea6a9c4f1a0eb29af3a436fecf4
Nathan Bijnens <nathan@nathan.gs>
2025-03-19 19:45:37 +0100

    HA: indego lawn mower

--- smarthome/appliances/indego.nix
+++ smarthome/appliances/indego.nix
@@ -12,13 +12,11 @@
               (ha.trigger.above ''sensor.indego_325608617_battery_percentage'' 90) 
               (ha.trigger.template_for ''{{ states('weather.sxw') not in ['rainy', 'pouring', 'lightning', 'lightning-rainy', 'snowy', 'snowy-rainy', 'hail', 'exceptional'] }}'' "01:00:00" )
               (ha.trigger.above "sensor.outdoor_temperature" 7)
-              (ha.trigger.at "10:00:00")
-              (ha.trigger.at "12:30:00")
-              (ha.trigger.at "16:00:00")
+              (ha.trigger.time_pattern_minutes "15")
             ];
             conditions = [
               (ha.condition.on "binary_sensor.indego_325608617_online")
-              (ha.condition.above ''sensor.indego_325608617_battery_percentage'' 90)
+              (ha.condition.above ''sensor.indego_325608617_battery_percentage'' 85)
               (ha.condition.template ''{{ states('weather.sxw') not in ['rainy', 'pouring', 'lightning', 'lightning-rainy', 'snowy', 'snowy-rainy', 'hail', 'exceptional'] }}'')
               (ha.condition.state "sun.sun" "above_horizon")
               (ha.condition.template ''{{ now().weekday() != 6 }}'')
@@ -32,27 +30,32 @@
               (ha.condition.above "sensor.outdoor_temperature" 7)
               (ha.condition.state "lawn_mower.indego_325608617" "docked")
               (
-                # Only run if not completed in last 3d
+                # Only run if not completed in last 12h
                 ha.condition.template ''
                   {% set last_completed = states('sensor.indego_325608617_last_completed') %}
-                  {{ last_completed != 'unknown' and (as_timestamp(now()) - as_timestamp(last_completed)) > (3 * 24 * 60 * 60) }}
+                  {{ last_completed != 'unknown' and (as_timestamp(now()) - as_timestamp(last_completed)) > (12 * 60 * 60) }}
                 ''
               )
             ];
             actions = [
               {
                 service = "lawn_mower.start_mowing";                
-                target.device_id = "lawn_mower.indego_325608617";
+                target.entity_id = "lawn_mower.indego_325608617";
               }
             ];
-          }
+          } 
+          
       )
       (
         ha.automation "garden/lawn_mower.turn_off"
           {
             triggers = [
               (ha.trigger.state_to "weather.sxw" ["rainy" "pouring" "lightning" "lightning-rainy" "snowy" "snowy-rainy" "hail" "exceptional"] )
-              (ha.trigger.state_to "sun.sun" "below_horizon")              
+              (ha.trigger.state_to "sun.sun" "below_horizon")
+              (ha.trigger.template ''
+                {% set last_completed = states('sensor.indego_325608617_last_completed') %}
+                  {{ last_completed != 'unknown' and (as_timestamp(now()) - as_timestamp(last_completed)) < (12 * 60 * 60) }}
+              '')              
             ];
             conditions = [
               (ha.condition.on "binary_sensor.indego_325608617_online")
@@ -61,7 +64,7 @@
             actions = [
               {
                 service = "lawn_mower.dock";                
-                target.device_id = "lawn_mower.indego_325608617";
+                target.entity_id = "lawn_mower.indego_325608617";
               }
             ];
           }
@@ -78,8 +81,8 @@
         ];
 
         entity_globs = [
-          "binary_sensor.indego_325608617_"
-          "sensor.indego_325608617_"
+          "binary_sensor.indego_325608617_*"
+          "sensor.indego_325608617_*"
         ];
       };
     };
