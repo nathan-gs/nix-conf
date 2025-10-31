3bbf9cdbe968508e4445d3b7c4a3ceec170875a7
Nathan Bijnens <nathan@nathan.gs>
2025-10-31 21:28:33 +0100

    vscode: expose dev as well

--- services/vscode.nix
+++ services/vscode.nix
@@ -48,5 +48,28 @@
     };
   };
 
+  services.nginx.virtualHosts."dev.nathan.gs" = {
+    forceSSL = true;
+    enableACME = true;
+    extraConfig = ''
+      proxy_buffering off;
+    '';
+    basicAuth = config.secrets.nginx.basicAuth;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:4000";
+      proxyWebsockets = true; # needed if you need to use WebSocket
+      extraConfig = ''
+        # required when the target is also TLS server with multiple hosts
+        proxy_ssl_server_name on;
+        # required when the server wants to use HTTP Authentication
+        proxy_pass_header Authorization;
+
+        proxy_set_header X-Real-IP          $remote_addr;
+        proxy_set_header X-Forwarded-For    $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto  $scheme;
+      '';
+    };
+  };
+
   networking.firewall.allowedTCPPorts = [ 3000 4000 ];
 }
