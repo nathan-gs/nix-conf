72def9003949226d77e41a5cc5c2d05a5a5bf5f0
Nathan Bijnens <nathan@nathan.gs>
2026-03-07 19:51:58 +0100

    HA: tv tweak

--- smarthome/appliances/tv.nix
+++ smarthome/appliances/tv.nix
@@ -9,7 +9,7 @@
           (ha.trigger.on "binary_sensor.floor0_living_appletv_woonkamer")          
         ];
         conditionsOn = [
-          (ha.condition.time_after "10:30:00")
+          (ha.condition.template "{{ (now().strftime('%H:%M') >= '10:30' and now().strftime('%H:%M') <= '17:30') or now().strftime('%H:%M') >= '20:30' }}")
         ];
         triggersOff = [
           (ha.trigger.off_for "binary_sensor.floor0_living_appletv_woonkamer" "00:02:00")
