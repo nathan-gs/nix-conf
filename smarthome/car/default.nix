23591a3b737c162c73fc8136bd454d92ee4797ca
Nathan Bijnens <nathan@nathan.gs>
2026-05-31 11:54:09 +0200

    HA: car add monthly & yearly sensor

--- smarthome/car/default.nix
+++ smarthome/car/default.nix
@@ -1,8 +1,16 @@
-{ ... }:
+{ ha, ... }:
 {
 
   imports = [
     ./car_charger
   ];
+
+  services.home-assistant.config = {
+    # Monthly and yearly utility meters for the Audi mileage sensor (km)
+    utility_meter = 
+      (ha.utility_meter "audi_a6_sportback_e_tron_mileage" "sensor.audi_a6_sportback_e_tron_mileage" "monthly")
+      // 
+      (ha.utility_meter "audi_a6_sportback_e_tron_mileage" "sensor.audi_a6_sportback_e_tron_mileage" "yearly");
+  };
 
 }
